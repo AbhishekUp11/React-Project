29ae0c9381195224f45ef895c4257ec1f67152db
abhishek upadhyay <a.upadhyay@iiitmanipur.ac.in>
2023-09-08 23:43:45 +0530

    class-component-usestate
    eg of class component and useState with increase counter with button click

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User'
+import {useState} from 'react';
 
 function App() {
+  const [data, setData] = useState(0);
+  // function setData(){
+  //   data = data+1;
+  // }
+  function setValue(){
+     setData(data+1);
+  }
   return (
-    <h1>Hello Abhi</h1>
+    <div className= "App">
+   <h1>Hello Abhi {data}</h1>
+   {/* <button onClick={()=>setData(data+1)}>Click Me</button> */}
+   <button onClick={()=>setValue(data+1)}>Click Me</button>
+   <User/>
+   </div>
   );
 }
 
